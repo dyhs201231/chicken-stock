578225ae16a49a9837b13a1d09d5325ac4814a8b
HyunSu <nnswwd@gmail.com>
2026-05-11 17:54:29 +0900

    [Feature] 아바타

---
+++ app/(frontend)/components/avatar/index.tsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+
+export type AvatarType =
+  | "header"
+  | "main"
+  | "portfolio-mini-circle"
+  | "portfolio-mini-square"
+  | "stock-detail"
+  | "mypage";
+
+export interface AvatarProps {
+  src: string;
+  alt?: string;
+  type: AvatarType;
+}
+
+const avatarClassName: Record<AvatarType, string> = {
+  header: "h-[50px] w-[50px] rounded-full",
+  main: "h-[30px] w-[30px] rounded-full",
+  "portfolio-mini-circle": "h-[40px] w-[40px] rounded-full",
+  "portfolio-mini-square": "h-[80px] w-[80px]",
+  "stock-detail": "h-[100px] w-[100px]",
+  mypage: "h-[100px] w-[100px] rounded-full",
+};
+
+const avatarLoading: Record<AvatarType, "lazy" | "eager"> = {
+  header: "eager",
+  main: "eager",
+  "portfolio-mini-circle": "lazy",
+  "portfolio-mini-square": "lazy",
+  "stock-detail": "eager",
+  mypage: "eager",
+};
+
+const Avatar = ({ src, type, alt }: AvatarProps) => {
+  return (
+    <div
+      className={`relative overflow-hidden ${avatarClassName[type]}`}
+      data-testid={`avatar-${type}`}
+    >
+      <Image
+        className="object-cover"
+        src={src}
+        alt={alt ?? src}
+        fill
+        loading={avatarLoading[type]}
+        unoptimized
+      />
+    </div>
+  );
+};
+
+export default Avatar;
---
+++ app/(frontend)/components/avatar/sample.tsx
@@ -0,0 +1,20 @@
+import Avatar from ".";
+
+const AvatarSample = () => {
+  return (
+    <div className="flex flex-col gap-4 rounded border p-5">
+      <h1>Avatar Sample</h1>
+
+      <div className="flex gap-5">
+        <Avatar src="/test-image.png" type="header" />
+        <Avatar src="/test-image.png" type="main" />
+        <Avatar src="/test-image.png" type="portfolio-mini-circle" />
+        <Avatar src="/test-image.png" type="portfolio-mini-square" />
+        <Avatar src="/test-image.png" type="stock-detail" />
+        <Avatar src="/test-image.png" type="mypage" />
+      </div>
+    </div>
+  );
+};
+
+export default AvatarSample;
